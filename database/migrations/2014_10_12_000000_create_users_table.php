2762751d469ab01a1646ce94084fa0a6259b5cae
David Kirarit <davidkrarit@gmail.com>
2021-06-20 14:46:40 +0300

    database factory seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string("last_name")->nullable();
             $table->string("full_name")->nullable();
             $table->string("email")->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string("password");
             $table->string("phone")->nullable();
             $table->timestamps();
